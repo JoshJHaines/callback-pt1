a2a8acbae32ae74bcbeee9842da444e35ecde26c
JoshuaJHaines <joshjhaines3@gmail.com>
2021-09-08 12:07:42 -0400

    saving

--- functions.js
+++ functions.js
@@ -3,7 +3,7 @@ const identity = function (value) {
 };
 const first = function (array, n) {
 	let newArr = [];
-	n;
+
 	if (n == undefined) {
 		return array[0];
 	}
@@ -17,7 +17,30 @@ const first = function (array, n) {
 	return newArr;
 };
 
-const last = function (array, n) {};
+const last = function (array, n) {
+  let newArr = [];
+  
+	if (n == undefined) {
+		return array[array.length-1];
+	}
+	else {
+    for (let i = array.length-1; newArr.length = 2; i--) {
+		if (array[i - 1] != undefined) {
+			newArr.push(array[i - 1]);
+		}
+	}
+}
+	return newArr;
+};
+
+test = last([1,2,3])
+test
+
+test2 = last([1,2,3], 1)
+test2
+
+test3 = last([1,2,3], 3)
+test3
 
 const each = function (collection, callback) {};
 const indexOf = function (array, target) {};
