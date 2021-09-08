177b8263960170ed2021686bffe6fdfc3d57f893
JoshuaJHaines <joshjhaines3@gmail.com>
2021-09-08 11:54:02 -0400

    All tests for 'First' function passing

--- functions.js
+++ functions.js
@@ -1,30 +1,22 @@
 const identity = function (value) {
-  return value;
+	return value;
 };
 const first = function (array, n) {
-  let newArr = []
-  n
-
-    for (let i = 0; i <= n; i++){
-      if (n == undefined){
-        newArr.push(array[0])
-      }
-      else if (array[i-1] != undefined){
-        newArr.push(array[i-1])
-      }
-
-  }
-  return newArr
+	let newArr = [];
+	n;
+	if (n == undefined) {
+		return array[0];
+	}
+	else {
+    for (let i = 0; i <= n; i++) {
+		if (array[i - 1] != undefined) {
+			newArr.push(array[i - 1]);
+		}
+	}
+}
+	return newArr;
 };
 
-test = first([1,2,3])
-test
-
-test2 = first([1,2,3], 1)
-test2
-
-test3 = first([1,2,3], 3)
-test3
 const last = function (array, n) {};
 
 const each = function (collection, callback) {};
@@ -32,10 +24,10 @@ const indexOf = function (array, target) {};
 const map = function (collection, iterator) {};
 
 module.exports = {
-  identity,
-  first,
-  last,
-  each,
-  indexOf,
-  map,
+	identity,
+	first,
+	last,
+	each,
+	indexOf,
+	map,
 };
