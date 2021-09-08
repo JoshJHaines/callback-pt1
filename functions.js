961a3a252017af94d98f3819c2c25026bb1d96bf
JoshuaJHaines <joshjhaines3@gmail.com>
2021-09-08 11:49:26 -0400

    3 of 4 tests passing on 'First' function. continuing on and circling back.

--- functions.js
+++ functions.js
@@ -1,8 +1,30 @@
 const identity = function (value) {
   return value;
 };
-const first = function (array, n) {};
+const first = function (array, n) {
+  let newArr = []
+  n
 
+    for (let i = 0; i <= n; i++){
+      if (n == undefined){
+        newArr.push(array[0])
+      }
+      else if (array[i-1] != undefined){
+        newArr.push(array[i-1])
+      }
+
+  }
+  return newArr
+};
+
+test = first([1,2,3])
+test
+
+test2 = first([1,2,3], 1)
+test2
+
+test3 = first([1,2,3], 3)
+test3
 const last = function (array, n) {};
 
 const each = function (collection, callback) {};
